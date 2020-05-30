11bbb5dd934de6eb9fd0e7d7679a9d40fdc3dbc4
Dhruv Kapur <dhruvkapur@Dhruvs-MacBook-Pro.local>
2020-05-30 13:31:00 +0530

    Also see the pointer sizes

--- c-primer/sizes.c
+++ c-primer/sizes.c
@@ -8,22 +8,40 @@
 
 int main() {
   PRINT_SIZE(int)
+  PRINT_SIZE(int *)
+  PRINT_SIZE(int * *)
   PRINT_SIZE(short)
+  PRINT_SIZE(short *)
   PRINT_SIZE(long)
+  PRINT_SIZE(long *)
   PRINT_SIZE(unsigned int)
+  PRINT_SIZE(unsigned int *)
   PRINT_SIZE(long long)
+  PRINT_SIZE(long long *)
   PRINT_SIZE(char)
+  PRINT_SIZE(char *)
   PRINT_SIZE(float)
+  PRINT_SIZE(float *)
   PRINT_SIZE(double)
-  PRINT_SIZE(uint8_t) // unsigned char = 8 bits
-  PRINT_SIZE(uint16_t) // unsigned short = 16 bits
-  PRINT_SIZE(uint32_t) // unsigned int = 32 bits
-  PRINT_SIZE(uint_fast8_t) // probably same as uint8_t
-  PRINT_SIZE(uint_fast16_t) // probably same as unit16_t
-  PRINT_SIZE(uintmax_t) // unsigned long
-  PRINT_SIZE(intmax_t) // simply a long?
-  PRINT_SIZE(__int128) // A 16 byte long integer!
-  PRINT_SIZE(int[2]) // Can query array directly
+  PRINT_SIZE(double *)
+  PRINT_SIZE(uint8_t)
+  PRINT_SIZE(uint8_t *) // unsigned char = 8 bits
+  PRINT_SIZE(uint16_t)
+  PRINT_SIZE(uint16_t *) // unsigned short = 16 bits
+  PRINT_SIZE(uint32_t)
+  PRINT_SIZE(uint32_t *) // unsigned int = 32 bits
+  PRINT_SIZE(uint_fast8_t)
+  PRINT_SIZE(uint_fast8_t *) // probably same as uint8_t
+  PRINT_SIZE(uint_fast16_t)
+  PRINT_SIZE(uint_fast16_t *) // probably same as unit16_t
+  PRINT_SIZE(uintmax_t)
+  PRINT_SIZE(uintmax_t *) // unsigned long
+  PRINT_SIZE(intmax_t)
+  PRINT_SIZE(intmax_t *) // simply a long?
+  PRINT_SIZE(__int128)
+  PRINT_SIZE(__int128 *) // A 16 byte long integer!
+  PRINT_SIZE(int[2])
+  // PRINT_SIZE(* int[2]) // Can query array directly
   
   typedef struct {
     int id;
